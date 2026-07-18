94471208fd74fcddf7d9514eb1137d0d7da1ce32
DevAyushKumar <blaze.main.id@gmail.com>
2026-07-18 18:44:46 +0530

    Modules and pip

---
+++ Lecture/modulesAndPip.py
@@ -0,0 +1,12 @@
+# What is module ?
+# => Module is like a code library which can be used to borrow the code written by somebody else in our python program. There are two types of modules in python :
+# 1. Built in modules : These modules are ready to import and use and ship with the python interpretor. There is no need to install such modules explicitely.
+# 2. External moduules : These modules are imported from a third party file or can be installed using package manager like pip or conda. Since the code is written by someone else, we can install different version of a same module with time.
+
+# What is pip ?
+# => It can be used as a package manager pip to install python module. 
+# Ex: pip install pandas
+
+# Using a module in python 
+import pandas
+pandas.read_csv("words.csv")
