927ef6d8a61dbe7185aa86cc3fb7b4f6be3113f1
DevAyushKumar <blaze.main.id@gmail.com>
2026-07-18 19:05:25 +0530

    Added comments

---
+++ Lecture/comments.py
@@ -0,0 +1,7 @@
+# What are comments ?
+# => A comment is a part of the coding file that programmer does not want to execute, rather than the programmer uses it to either explain a block of code or to avoid the execution of a specific part of the code while testing.
+# Single line comment: To write comment just add '#' at the start of the line to start single line comment.
+
+# What are escape sequence characters ?
+# => To insert characters that cannot be directly used in a string, we use an escape sequece characters.
+# An escape sequence character is backlash \ followed by the character you want to insert.
