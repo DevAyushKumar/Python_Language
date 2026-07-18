a2300db89c346ca76ac3d796599b9a645aa3c370
DevAyushKumar <blaze.main.id@gmail.com>
2026-07-18 17:57:58 +0530

    Intro for python

---
+++ Lecture/intro.py
@@ -0,0 +1,18 @@
+# What is python ?
+# => ~ Python is dynamically typed, general purpose programming language that supports an object oriented programming approach as well as functional programming apporach.
+# ~ python is interpreted and a high level programming language.
+
+# Features of python ?
+# => ~ python is simple and easy to understand.
+# ~ Python is interpreted and platform independent which makes debugging very easy
+# ~ It is interpreted and platform-independent which makes debugging very easy.
+# ~ python is an open source programming language.
+# ~ python provides very big library support. Some of the popular libraries including NumPy, Tesnserflow, OpenCV, etc.
+# ~ It is possible to integrate other programming languages with python.
+
+# What python is used for ?
+# => ~ Python is used in data visualiation to create plots and graphical representation.
+# ~ Python helps in data analytics to analyze and understand raw data for insights and trends.
+# ~ It is used in AI and Machine Learning to simluate human behaviour and to learn from past data without hard coding.
+# ~ It is used to create web applications.
+# ~ It is used in business and accounting to perform complex mathematical operations along with quantitative and quantitative analysis.
