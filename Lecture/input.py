2c2c23ff372ca7adc9bdd96ebcf4f08c21866524
DevAyushKumar <blaze.main.id@gmail.com>
2026-07-19 19:00:59 +0530

    Input in python

---
+++ Lecture/input.py
@@ -0,0 +1,11 @@
+''' TAKING INPUT FROM USER
+In python, we can take input directly from the user by using the input() function. This input function gives a return value as string/character hence we have to pass that into a variable. 
+
+Syntax:
+variable = input()
+
+But input function returns the value as a string. Hence we have to typecaste them whenever required to another datatype'''
+
+a=int(input("As an integer"))
+b=float(input("As a float"))
+b=input("As a string")
