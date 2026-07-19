bc9cacd17468b11348a8678b7be51994302e15c5
DevAyushKumar <blaze.main.id@gmail.com>
2026-07-19 17:52:40 +0530

    Varaibles and datatypes

---
+++ Lecture/variableAndDatatypes.py
@@ -0,0 +1,25 @@
+# What is variable ?
+# => Varaible is a container that holdes a value.
+# Types of variables:
+# 1.integer
+# 2.boolean
+# 3.string
+# 4.float
+
+# What is dataTypes ?
+# => Data types specifies the type of value a varaible holds. This is required in programming to do various operations without causing an error.
+# By default python provides these datatypes: 
+# 1. Numeric data: int, float, complex
+a = 3, b = 3.4
+# 2. Text data: str
+name = 'Ayush'
+# 3. Boolean data
+bool = True
+# 4. Sequenced data list 
+# => List: A list an ordered collection of data seperated by a comma and enclosed within square bracket. Lists are mutable and can be modified after creation
+list1= ['Apple', 'Bannana', 12, 13]
+# => Tuple: A tuple is an ordered list of collection of data with elements seperated by a comma and enclosed within paranthesis. Tuples are immutable and cannot be modified after creation.
+tuple1 = (['First', 'Second', 'Third'])
+# 5. Mapped data: Dictionary
+# Dict: A dictionary is an unordered colletion of data containing key:value pair. The key:value pairs are enclosed within curly brackets.
+dict1 = {'Name': 'Ayush', 'Age': 18, 'Role': 'Student'}
