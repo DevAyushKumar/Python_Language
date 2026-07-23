a7d8f08971bfd9543d22c440f6a7da1b78654210
DevAyushKumar <blaze.main.id@gmail.com>
2026-07-23 19:30:55 +0530

    Dictionaries

---
+++ Documentation/dictionaries.py
@@ -0,0 +1,7 @@
+'''Python dictionaries:
+Dictionaries are ordered collection of data items. They are store multiple items in a single variable. Dictionary items are key-value pairs that are seperated by commas and enclosed within curly brackets{}'''
+info={"name":"karan", "age":19, "eligible":True}
+
+#Accessing dictionary items
+'''1. Accessing single values:
+Values in a dictionary can be accessed using keys. We can access dictionary values by mentioning keys either in square brackets or by using get meathod.'''
